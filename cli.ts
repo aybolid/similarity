7f7c4fe38ace43c40d0692373172f5050243e96b
matth <matthewiva13@gmail.com>
2025-04-25 23:36:54 +0300

    feat: remove comments

--- cli.ts
+++ cli.ts
@@ -15,29 +15,3 @@ cmd.parseArgs(args);
 const now = performance.now();
 await cmd.run();
 console.log(`Done in ${((performance.now() - now) / 1000).toFixed(2)}s`);
-
-// const inputEmbedding = await generateEmbedding(input);
-
-// const similarity = sql<number>`1 - (${cosineDistance(docs.embedding, inputEmbedding)})`;
-
-// const similarDocs = await db
-//   .select({
-//     id: docs.id,
-//     name: docs.title,
-//     content: docs.content,
-//     similarity,
-//   })
-//   .from(docs)
-//   .where(gt(similarity, 0.78))
-//   .orderBy((t) => desc(t.similarity))
-//   .limit(2);
-
-// if (!similarDocs.length) {
-//   console.log("No similar docs found.");
-// } else {
-//   console.log("Similar docs:");
-//   for (const doc of similarDocs) {
-//     console.log(`- ${doc.name} (similarity: ${doc.similarity.toFixed(2)})`);
-//     console.log(`  ${doc.content}`);
-//   }
-// }
